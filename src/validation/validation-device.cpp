d5e902bdce7eb645497dd4bd02e4b921f121e284
Gael Cathelin <gael.cathelin@gmail.com>
2025-01-10 12:04:29 +0100

    [Validation] Allow ranges in VolatileConstantBuffers

--- src/validation/validation-device.cpp
+++ src/validation/validation-device.cpp
@@ -1607,7 +1607,7 @@ namespace nvrhi::validation
                     return false;
                 }
             }
-
+/*
             if (binding.type == ResourceType::VolatileConstantBuffer && !binding.range.isEntireBuffer(desc))
             {
                 const BufferRange range = binding.range.resolve(desc);
@@ -1615,7 +1615,7 @@ namespace nvrhi::validation
                     << " is bound with effective byteOffset = " << range.byteOffset << ", byteSize = " << range.byteSize << ".";
                 return false;
             }
-
+//*/
             break;
         }
 
